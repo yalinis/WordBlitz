418f1117d396d43646e0c3cfb81bd55fc8a54d0d
yalinis <145574629+yalinis@users.noreply.github.com>
2024-05-06 09:24:14 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hi")
+import pygame
+
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Verdana', 20)
+my_font_big = pygame.font.SysFont('Verdana', 30)
+
+# set up variables for the display
+size = (1200, 600)
+screen = pygame.display.set_mode(size)
+
+bg = pygame.image.load("background.jpg")
+
+run = True
+# -------- Main Program Loop -----------
+while run:
+    # --- Main event loop
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+    screen.blit(bg, (0, 0))
+    pygame.display.update()
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
