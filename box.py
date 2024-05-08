9b0cb9a1d463c1fb6435e63c9a0f295065f332ef
yalinis <145574629+yalinis@users.noreply.github.com>
2024-05-08 09:27:43 -0400

    Add files via upload

---
+++ box.py
@@ -0,0 +1,31 @@
+import pygame
+
+class Box:
+
+    def __init__(self, x, y, box_type):
+        self.box_type = box_type
+        self.x = x
+        self.y = y
+        self.set_image()
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def uncover_box(self):
+        self.box_type -= 1
+        if self.box_type < 0:
+            self.box_type = 1
+        self.set_image()
+
+    def set_image(self):
+        if self.box_type == 0:
+            self.image = pygame.image.load("yellow-block.png")
+            self.image_size = self.image.get_size()
+            scale_size = (self.image_size[0] * .035, self.image_size[1] * .035)
+            self.image = pygame.transform.scale(self.image, scale_size)
+            self.image_size = self.image.get_size()
+        else:
+            self.image = pygame.image.load("orangeblock.png")
+            self.image_size = self.image.get_size()
+            scale_size = (self.image_size[0] * .21, self.image_size[1] * .21)
+            self.image = pygame.transform.scale(self.image, scale_size)
+            self.image_size = self.image.get_size()
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-from box_cover import Box_cover
-from box_empty import Box_empty
+from box import Box
 
 pygame.init()
 pygame.font.init()
@@ -8,18 +7,12 @@ my_font = pygame.font.SysFont('Verdana', 20)
 my_font_big = pygame.font.SysFont('Verdana', 30)
 
 # set up variables for the display
-size = (1200, 600)
+size = (1000, 1000)
 screen = pygame.display.set_mode(size)
 
 bg = pygame.image.load("background.jpg")
 
-
-bcw1 = Box_cover(500, 250)
-bew1 = Box_empty(500, 250)
-bcw2 = Box_cover(575, 250)
-bew2 = Box_empty(575, 250)
-bcw3 = Box_cover(650, 250)
-bew3 = Box_empty(650, 250)
+b1 = Box(300, 400, 1)
 
 
 run = True
@@ -31,13 +24,12 @@ while run:
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
 
+        if event.type == pygame.MOUSEBUTTONUP:
+            b1.uncover_box()
+
+
     screen.blit(bg, (0, 0))
-    screen.blit(bew1.image, bew1.rect)
-    screen.blit(bcw1.image, bcw1.rect)
-    screen.blit(bew2.image, bew2.rect)
-    screen.blit(bcw2.image, bcw2.rect)
-    screen.blit(bew3.image, bew3.rect)
-    screen.blit(bcw3.image, bcw3.rect)
+    screen.blit(b1.image, b1.rect)
     pygame.display.update()
 
 # Once we have exited the main program loop we can stop the game engine:
